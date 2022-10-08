dd25d2decc1215bc47a809cc27db7be8001cd2e6
DESKTOP-R1885O4\Admin <luigizambeletti@libero.it>
2022-10-08 12:39:50 +0200

    Added Wall Stick time to the Move script

--- Assets/Scripts/Capabilities/Move.cs
+++ Assets/Scripts/Capabilities/Move.cs
@@ -8,36 +8,62 @@ namespace Shinjingi
         [SerializeField, Range(0f, 100f)] private float _maxSpeed = 4f;
         [SerializeField, Range(0f, 100f)] private float _maxAcceleration = 35f;
         [SerializeField, Range(0f, 100f)] private float _maxAirAcceleration = 20f;
+        [SerializeField, Range(0.05f, 0.5f)] private float _wallStickTime = 0.25f;
 
         private Controller _controller;
         private Vector2 _direction, _desiredVelocity, _velocity;
         private Rigidbody2D _body;
-        private CollisionDataRetriever _ground;
+        private CollisionDataRetriever _collisionDataRetriever;
+        private WallInteractor _wallInteractor;
 
-        private float _maxSpeedChange, _acceleration;
+        private float _maxSpeedChange, _acceleration, _wallStickCounter;
         private bool _onGround;
 
         private void Awake()
         {
             _body = GetComponent<Rigidbody2D>();
-            _ground = GetComponent<CollisionDataRetriever>();
+            _collisionDataRetriever = GetComponent<CollisionDataRetriever>();
             _controller = GetComponent<Controller>();
+            _wallInteractor = GetComponent<WallInteractor>();
         }
 
         private void Update()
         {
             _direction.x = _controller.input.RetrieveMoveInput();
-            _desiredVelocity = new Vector2(_direction.x, 0f) * Mathf.Max(_maxSpeed - _ground.Friction, 0f);
+            _desiredVelocity = new Vector2(_direction.x, 0f) * Mathf.Max(_maxSpeed - _collisionDataRetriever.Friction, 0f);
         }
 
         private void FixedUpdate()
         {
-            _onGround = _ground.OnGround;
+            _onGround = _collisionDataRetriever.OnGround;
             _velocity = _body.velocity;
 
             _acceleration = _onGround ? _maxAcceleration : _maxAirAcceleration;
             _maxSpeedChange = _acceleration * Time.deltaTime;
             _velocity.x = Mathf.MoveTowards(_velocity.x, _desiredVelocity.x, _maxSpeedChange);
+
+            #region Wall Stick
+            if(_collisionDataRetriever.OnWall && !_collisionDataRetriever.OnGround && !_wallInteractor.WallJumping)
+            {
+                if(_wallStickCounter > 0)
+                {
+                    _velocity.x = 0;
+
+                    if(_controller.input.RetrieveMoveInput() == _collisionDataRetriever.ContactNormal.x)
+                    {
+                        _wallStickCounter -= Time.deltaTime;
+                    }
+                    else
+                    {
+                        _wallStickCounter = _wallStickTime;
+                    }
+                }
+                else
+                {
+                    _wallStickCounter = _wallStickTime;
+                }
+            }
+            #endregion
 
             _body.velocity = _velocity;
         }
