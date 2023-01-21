5944c1dc94e075022e1fa05e5db6dece4c1cfff3
DESKTOP-R1885O4\Admin <luigizambeletti@libero.it>
2023-01-21 18:25:10 +0100

    Added behavior to AIController movement method

--- Assets/Scripts/Controllers/AIController.cs
+++ Assets/Scripts/Controllers/AIController.cs
@@ -5,17 +5,42 @@ namespace Shinjingi
     [CreateAssetMenu(fileName = "AIController", menuName = "InputController/AIController")]
     public class AIController : InputController
     {
-        public override bool RetrieveJumpInput()
+        [Header("Interaction")]
+        [SerializeField] private LayerMask _layerMask = -1;
+        [Header("Ray")]
+        [SerializeField] private float _bottomDistance = 1f;
+        [SerializeField] private float _topDistance = 1f;
+        [SerializeField] private float _xOffset = 1f;
+
+        private RaycastHit2D _groundInfoBottom;
+        private RaycastHit2D _groundInfoTop;
+
+        public override bool RetrieveJumpInput(GameObject gameObject)
         {
-            return true;
+            return false;
         }
 
-        public override float RetrieveMoveInput()
+        public override float RetrieveMoveInput(GameObject gameObject)
         {
-            return 1f;
+            _groundInfoBottom = Physics2D.Raycast(new Vector2(gameObject.transform.position.x + (_xOffset * gameObject.transform.localScale.x),
+                gameObject.transform.position.y), Vector2.down, _bottomDistance, _layerMask);
+            Debug.DrawRay(new Vector2(gameObject.transform.position.x + (_xOffset * gameObject.transform.localScale.x), gameObject.transform.position.y),
+                Vector2.down * _bottomDistance, Color.green);
+
+            _groundInfoTop = Physics2D.Raycast(new Vector2(gameObject.transform.position.x + (_xOffset * gameObject.transform.localScale.x),
+                gameObject.transform.position.y), Vector2.right * gameObject.transform.localScale.x, _topDistance, _layerMask);
+            Debug.DrawRay(new Vector2(gameObject.transform.position.x + (_xOffset * gameObject.transform.localScale.x), gameObject.transform.position.y),
+                Vector2.right * _topDistance * gameObject.transform.localScale.x, Color.green);
+
+            if(_groundInfoTop.collider == true || _groundInfoBottom.collider == false)
+            {
+                gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x * -1, gameObject.transform.localScale.y);
+            }
+
+            return gameObject.transform.localScale.x;
         }
 
-        public override bool RetrieveJumpHoldInput()
+        public override bool RetrieveJumpHoldInput(GameObject gameObject)
         {
             return false;
         }
