547942eaaccba2dd1e43b91bd5f3eda15307517f
DESKTOP-R1885O4\Admin <luigizambeletti@libero.it>
2023-06-24 12:02:23 +0200

    Modified PlayerController to use Input Actions from the new Input System

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -5,19 +5,43 @@ namespace Shinjingi
     [CreateAssetMenu(fileName = "PlayerController", menuName = "InputController/PlayerController")]
     public class PlayerController : InputController
     {
+        private PlayerInputActions _inputActions;
+        private bool _isJumping;
+
+        private void OnEnable()
+        {
+            _inputActions = new PlayerInputActions();
+            _inputActions.Gameplay.Enable();
+            _inputActions.Gameplay.Jump.started += JumpStarted;
+            _inputActions.Gameplay.Jump.canceled += JumpCanceled;
+        }
+
+        private void OnDisable()
+        {
+            _inputActions.Gameplay.Disable();
+            _inputActions.Gameplay.Jump.started -= JumpStarted;
+            _inputActions.Gameplay.Jump.canceled -= JumpCanceled;
+            _inputActions = null;
+        }
+
+        private void JumpCanceled(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+        {
+            _isJumping = false;
+        }
+
+        private void JumpStarted(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+        {
+            _isJumping = true;
+        }
+
         public override bool RetrieveJumpInput(GameObject gameObject)
         {
-            return Input.GetButtonDown("Jump");
+            return _isJumping;
         }
 
         public override float RetrieveMoveInput(GameObject gameObject)
         {
-            return Input.GetAxisRaw("Horizontal");
-        }
-
-        public override bool RetrieveJumpHoldInput(GameObject gameObject)
-        {
-            return Input.GetButton("Jump");
+            return _inputActions.Gameplay.Move.ReadValue<Vector2>().x;
         }
     }
 }
